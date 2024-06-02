663ec772cd603cb12043e08c35e560bebb2558ca
1baiel1 <mukasevbajel6@gmail.com>
2024-06-02 16:23:48 +0600

    header jasadym

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,24 @@
+import "./header.css";
+import img1 from "../../images/logo.458f2cd.svg"
+
 function Header() {
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <header className="header">
+          <img className="header-title" src={img1} alt="" />
+          <nav className="header-nav">
+            <a href="#vacancies">Вакансии</a>
+            <a href="#events">Мероприятия</a>
+            <a href="#videos">Видео</a>
+            <a href="#organizations">Организации</a>
+            <a href="#community">Сообщество</a>
+          </nav>
+          <button className="header-btn">Войти</button>
+        </header>
+      </div>
+    </>
+  );
 }
 
 export default Header;
